016621677496157004de0b62c6d7385aed7e7c52
parhelium <kubawyrobek@fastmail.com>
2016-01-29 10:41:26 +0100

    further hints

---
+++ app/imageSlider.js
@@ -0,0 +1,7 @@
+import template from './imageSlider.html!text';
+
+ImageSlider = Ractive.extend({
+  template : template
+})
+
+Ractive.components.X = ImageSlider;
--- app/main.js
+++ app/main.js
@@ -2,6 +2,7 @@ import 'fetch'
 
 import Ractive from 'ractive'
 import template from './main.html!text'
+import imageSlider from './imageSlider'
 
 let ractive = new Ractive({
     el :'body',
